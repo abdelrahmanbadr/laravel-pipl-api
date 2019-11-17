2513b74848498428e7638cc0e584e382bf0f8059
Abdelrahman <abdelrahman.badreldeen@scratchpay.com>
2019-11-17 03:57:40 +0200

    Test: Add unit test for pipl search method

--- tests/PiplTest.php
+++ tests/PiplTest.php
@@ -1,7 +1,51 @@
 <?php
 
+namespace Tests;
 
-class PiplTest
+use Abdelrahman_badr\Pipl\Pipl;
+use GuzzleHttp\ClientInterface;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Exception;
+
+/**
+ * Class PiplTest
+ * @package Tests
+ */
+class PiplTest extends TestCase
 {
+    /**
+     * @var ClientInterface
+     */
+    protected $clientMock;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->clientMock = Mockery::mock(ClientInterface::class);
+    }
+
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
+    public function testPipleSearch()
+    {
+        $expectedResponse = "OK";
+        $this->clientMock->shouldReceive('getBody')->andReturn(json_encode($expectedResponse));
+        $this->clientMock->shouldReceive('get')
+            ->with("http://api.pipl.com/search/?key=&name=john")
+            ->andReturn($this->clientMock);
+        $pipl = new Pipl($this->clientMock);
+        $actualResponse = $pipl->search(["name" => "john"]);
+        $this->assertEquals($expectedResponse, $actualResponse);
+    }
+
+    public function testPipleSearchThrowException()
+    {
+        $this->expectException(Exception::class);
+        $pipl = new Pipl($this->clientMock);
+        $pipl->search([]);
+    }
 }
